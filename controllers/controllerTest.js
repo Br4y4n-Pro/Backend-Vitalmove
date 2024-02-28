fe6deb40ed8840649d602881f3d21949944b1bfa
L1S1M4C0 <itslisimaco@mail.com>
2024-02-28 17:16:42 -0500

    Lisimaco:Creacion del test caminata backend

--- controllers/controllerTest.js
+++ controllers/controllerTest.js
@@ -1,4 +1,36 @@
-export const crearCaminata =(req,res)=>{
+export const crearCaminata = async (req, res) => {
+  console.log(req.body);
 
+  if (
+    req.body.FCR == "" ||
+    req.body.TIEMPO == "" ||
+    req.body.DISTANCIA == "" ||
+    req.body.FCM == ""
+  ) {
+    console.log(`Llegó vacío`);
 
-}
+    return res.status(400).json({
+      mensaje: "Alguno de los campos está vacío",
+      rp: "no",
+    });
+  }
+
+  try {
+    const result = await crearTestCaminataModel(req.body);
+    console.log("Controlador:", result);
+    if (result.rp == "no") {
+      return res.status(200).json(result);
+    }
+    if (result instanceof Error) {
+      return res.status(401).json({ error: result.message });
+    } else {
+      res.status(200).json(result);
+    }
+  } catch (error) {
+    // Captura cualquier error que ocurra en crearTestCaminataModel y envía una respuesta adecuada
+    return res.status(203).json({
+      mensaje: "Error al enviar",
+      rp: "no",
+    });
+  }
+};
--- models/testVItalMoveModel.js
+++ models/testVItalMoveModel.js
@@ -1,15 +1,40 @@
+// import dotenv from "dotenv";
+// dotenv.config();
+// import pkg from "pg";
+// const { Pool } = pkg;
+// import { CONFIG_DB } from "../config/db.js";
+// const pool = new Pool(CONFIG_DB);
+
+// export const crearTestCaminataModel =(data) => {
+
+//     // pool.query('INSERT INTO caminatas (fcr, tiempo, distancia, fcm) VALUES ($1, $2, $3, $4)', [data.FCR, data.TIEMPO, data.DISTANCIA, data.FCM], (err, result) => {
+//     //         if (err) {
+//     //             console.error('Error al insertar en la base de datos:', err);
+//     //         } else {
+//     //             console.log('Datos insertados correctamente:', result.rows);
+//     //         }
+
+//             const query = 'INSERT INTO caminatas (fcr, tiempo, distancia, fcm) VALUES ($1, $2, $3, $4)';
+//             const values = [data.FCR, data.TIEMPO, data.DISTANCIA, data.FCM];
+
+//             return pool.query(query, values);
+//         };
+
 import dotenv from "dotenv";
-dotenv.config();
 import pkg from "pg";
-const { Pool } = pkg;
 import { CONFIG_DB } from "../config/db.js";
+
+// Configurar variables de entorno desde el archivo .env
+dotenv.config();
+
+const { Pool } = pkg;
 const pool = new Pool(CONFIG_DB);
 
+export const crearTestCaminataModel = (data) => {
+  // Ejemplo básico de inserción (recuerda manejar errores adecuadamente)
+  const query =
+    "INSERT INTO caminatas (fcr, tiempo, distancia, fcm) VALUES ($1, $2, $3, $4)";
+  const values = [data.FCR, data.TIEMPO, data.DISTANCIA, data.FCM];
 
-
-export const crearTestCaminataModel =(data) => {
-
-    cvodos = 20 
-    if
-
-}
+  return pool.query(query, values);
+};
